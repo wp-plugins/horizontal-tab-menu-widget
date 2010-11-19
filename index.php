b573b2f9e745762f0f936fc0d15fdee9034cbc80
anmari <anmari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-19 05:05:57 +0000

    attempt to remove
    git-svn-id: https://plugins.svn.wordpress.org/horizontal-tab-menu-widget/trunk@313229 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: tab menu navigation
+Version: 1.2
+Description:  This plugin is no longer particulary useful - there are better options out there.  I will attempt to get this removed.
+*/
+/*  Copyright 2009   
+	This program is distributed  WITHOUT ANY WARRANTY or GUARANTEE OF SUPPORT.
+	Support is usually available at http://webdesign.anmari.com/plugins/Impatient/ or at wordpress
+	You should test the functioning within your application yourself.
+	You may modify the code for your own purposes, you may not sell the code, or any modifcation based on the code. 
+*/
+
+?>
---
+++ readme.txt
@@ -0,0 +1,8 @@
+=== tab menu navigation ===
+Tested up to: 2.8.4
+Version: 1.2
+Stable tag: trunk
+
+== Description ==
+This plugin is no longer relevant - there are better options out there.  I will attempt to get this removed.
+
